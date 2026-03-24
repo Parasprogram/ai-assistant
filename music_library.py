66eca9d805be613409188d7b95ba024554d8c21a
Parasprogram <phirel2003@gmail.com>
2026-03-24 13:32:58 +0530

    Add files via upload

---
+++ music_library.py
@@ -0,0 +1,8 @@
+music= {
+    
+    "khaab":"https://www.youtube.com/watch?v=2eliQ_KR8yA&list=RD2eliQ_KR8yA&start_radio=1" ,
+    
+    "Fairy Tail":"https://www.youtube.com/watch?v=KgA2TpKJC30&list=RDKgA2TpKJC30&start_radio=1",
+    
+    "Aadat":"https://www.youtube.com/watch?v=e5LShHAE03A&list=RD47Tsuz5OhBU&index=6"
+}
